c027d0d40d1d605865ce7d4d18b55180375e4309
Drmrsan <drmrsan@gmail.com>
2017-02-20 23:34:37 +0100

    handle states, and calendar addon

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -7,17 +7,36 @@ var AppointmentForm = React.createClass({
 		this.props.onUserInput(obj);
 	},
 
+	handleSubmit: function(e) {
+		e.preventDefault();
+		this.props.onFormSubmit();
+	},
+
+	setApptTime: function(e){
+		var name = 'apt_time';
+		obj = {};
+		if (obj[name] = e.toDate()) {
+			this.props.onUserInput(obj);
+		}
+	},
+
 	render: function(){
+		var inputProps = {
+			name: 'apt_time'
+		};
+
 		return(
 			<div>
 				<h2>Make a new appointment</h2>
-				<form>
+				<form onSubmit={this.handleSubmit}>
 					<input name="title" placeholder="Appointment title" 
-						value={this.props.input_title}
+						value={this.props.title}
 						onChange={this.handleChange} />
-					<input name="apt_time" placeholder="Date and time" 
-						value={this.props.input_apt_time}
-						onChange={this.handleChange} />
+
+					<Datetime input={false} open={true}
+						inputProps={inputProps}
+						value={this.props.apt_time}
+						onChange={this.setApptTime} />
 					<input type="submit" value="Make Appointment" />
 				</form>
 			</div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -12,13 +12,32 @@ var Appointments = React.createClass({
 		this.setState(obj)
 	},
 
+	handleFormSubmit: function() {
+		var appointment = {title: this.state.title, apt_time: this.state.apt_time};
+		$.post('/appointments',
+						{appointment: appointment})
+					.done(function(data) {
+						this.addNewAppointment(data);
+					}.bind(this));
+	},
+
+	addNewAppointment: function(appointment){
+		var appointments = React.addons.update(this.state.appointments, {$push: [appointment]});
+		this.setState({ 
+			appointments: appointments.sort(function(a,b){
+				return new Date(a.apt_time) - new Date(b.apt_time);
+			})
+		});
+	},
+
 	render: function() {
 		return(
 			<div>
 				<AppointmentForm 
-					input_title={this.state.title} 
-					input_apt_time={this.state.apt_time}
-					onUserInput={this.handleUserInput} />
+					title={this.state.title} 
+					apt_time={this.state.apt_time}
+					onUserInput={this.handleUserInput}
+					onFormSubmit={this.handleFormSubmit} />
 				<AppointmentsList appointments={this.state.appointments} />
 			</div>
 		)
